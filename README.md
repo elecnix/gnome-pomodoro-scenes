c4b43a8157a74689d9b59da266dcb1cd3f3f4115
Nicolas Marchildon <nicolas@marchildon.net>
2023-10-22 23:22:30 -0400

    Add action script

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # gnome-pomodoro-scenes
+
+This script is used to control [Home Assistant](https://www.home-assistant.io/) [scenes](https://www.home-assistant.io/integrations/scene) for [gnome-pomodoro](https://gnomepomodoro.org/).
+
+When starting a pomodoro timer, you can enable a scene which you should configure in your Home Assistant. The scene could turn on a light bulb with a particular color.
+
+## Scenes
+
+These scene identifiers are expected, so make sure yours match:
+
+ - `scene.normal` when stopping the timer
+ - `pomodoro_enabled` when the timer is running
+ - `pomodoro_suspended` when the timer is paused (including during a break)
+ - `pomodoro_short_break` during a short break
+ - `pomodoro_long_break` during a long break
+
+## Configuration
+- Create a file named ~/.PomodoroHomeAssistant with a [long-lived access token](https://developers.home-assistant.io/docs/auth_api/#long-lived-access-token) and the URL to your Home Assistant server:
+
+```
+{
+    "config": "your-long",
+    "url": "your Home Assistant url"
+}
+```
+
+Test it with `./pomodoro.py pomodoro start`
+
+In Gnome Pomodoro, go to *Preferences*, *Custom Actions...*, click the + button, and:
+ - give it a name, like "Home Assistant"
+ - Enter the path to `pomodoro.py` followed by `$(state) $(triggers)`
+ - Enable all of Pomodoro, Short Break and Long Break
+ - Toggle on all the triggers
+
+![Gnome Pomodoro](gnome-pomodoro-action.png)
---
+++ pomodoro.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+"""
+Usage: python3 pomodoro.py <timer_type> <timer_action>
+
+Arguments:
+- timer_type: The type of timer to control (pomodoro, short-break, long-break)
+- timer_action: The action to perform on the timer (start, pause, resume, skip)
+"""
+from requests import post
+import syslog
+import json
+import sys
+import os
+
+# Read token and url from ~/.PomodoroHomeAssistant JSON file
+with open(os.path.expanduser("~/.PomodoroHomeAssistant")) as f:
+    config = json.load(f)
+    TOKEN = config["token"]
+    url = config["url"]
+
+# Check if token and url are valid
+if not TOKEN:
+    # log to syslog and raise exception
+    msg = "Missing token"
+    syslog.syslog(syslog.LOG_ERR, msg)
+    raise Exception(msg)
+if not url:
+    # log to syslog and raise exception
+    msg = "Missing url"
+    syslog.syslog(syslog.LOG_ERR, msg)
+    raise Exception(msg)
+
+# Define a mapping of program arguments to entity_id values
+argument_mapping = {
+    "pomodoro": {
+        "start": "scene.pomodoro_enabled",
+        "pause": "scene.pomodoro_suspended",
+        "resume": "scene.pomodoro_enabled",
+        "skip": "scene.normal",
+    },
+    "short-break": {
+        "start": "scene.pomodoro_pause_short",
+        "pause": "scene.pomodoro_suspended",
+        "resume": "scene.pomodoro_pause_short",
+        "skip": "scene.normal",
+    },
+    "long-break": {
+        "start": "scene.pomodoro_pause_long",
+        "pause": "scene.pomodoro_suspended",
+        "resume": "scene.pomodoro_pause_long",
+        "skip": "scene.normal",
+    },
+}
+
+url = f"{url}/api/services/scene/turn_on"
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "content-type": "application/json",
+}
+
+# check if arguments are valid
+if sys.argv[1] not in argument_mapping:
+    raise "Invalid program argument"
+if sys.argv[2] not in argument_mapping[sys.argv[1]]:
+    # check if arguments are valid
+    if sys.argv[1] not in argument_mapping:
+        msg = "Invalid program argument: " + sys.argv[1]
+        syslog.syslog(syslog.LOG_ERR, msg)
+        raise Exception(msg)
+    if sys.argv[2] not in argument_mapping[sys.argv[1]]:
+        msg = "Invalid program argument: "+ sys.argv[2]
+        syslog.syslog(syslog.LOG_ERR, msg)
+        raise Exception(msg)
+
+# Set the entity_id value from the argument mapping
+data = {}
+data["entity_id"] = argument_mapping[sys.argv[1]][sys.argv[2]]
+
+# append arguments to /tmp/pomo file
+with open("/tmp/pomo", "a") as f:
+    msg = f"{sys.argv[1:]} --> {data}\n"
+    f.write(msg)
+    syslog.syslog(syslog.LOG_ERR, msg)
+
+# Send the request
+response = post(url, headers=headers, json=data)
+print(response.text)
